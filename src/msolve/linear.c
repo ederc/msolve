a6ed8444d9e012fd11ab289790b8b39c451cc743
ederc <ederc@mathematik.uni-kl.de>
2026-09-15 11:36:36 +0200

    fixes linear poly setter for GF(16) and GF(256)

--- src/msolve/linear.c
+++ src/msolve/linear.c
@@ -425,6 +425,80 @@ static inline void check_and_set_linear_poly_32(nvars_t *nlins_ptr,
   lineqs_ptr[0] = lineqs;
 }
 
+static inline void check_and_set_linear_poly_gf_16_256(nvars_t *nlins_ptr,
+                                                nvars_t *linvars,
+                                                uint32_t **lineqs_ptr,
+                                                ht_t *bht, int32_t *bexp_lm,
+                                                bs_t *bs) {
+  long nlins = 0;
+  /*
+    la i-ieme entree de linvars est a 0 si il n'y a pas de forme lineaire dont
+    le terme dominant est vars[i].
+    Sinon, on met l'indice du polynome dans la base + 1.
+  */
+  for (long i = 0; i < bs->lml; i++) {
+    long deg = 0;
+    for (int j = 0; j < bht->nv; j++) {
+      deg += bexp_lm[i * bht->nv + j];
+    }
+
+    if (deg == 1) {
+      nlins++;
+      int linvarsxn = 1;
+      for (int k = 0; k < bht->nv-1; k++) {
+        if (bexp_lm[i * bht->nv + k] == 1) {
+          linvars[k] = i + 1;
+          linvarsxn = 0;
+          break;
+        }
+      }
+      if (linvarsxn){
+          nlins--;
+      }
+    }
+  }
+
+  *nlins_ptr = nlins;
+
+  // On recupere les coefficients des formes lineaires
+  uint32_t *lineqs = calloc(nlins * (bht->nv + 1), sizeof(uint32_t));
+  int cnt = 0;
+  for (int i = 0; i < bht->nv; i++) {
+    if (linvars[i] != 0) {
+
+      long len = bs->hm[bs->lmps[linvars[i] - 1]][LENGTH];
+
+      const bl_t bi = bs->lmps[linvars[i] - 1];
+      if (len == bht->nv + 1) {
+        for (long j = 0; j < len; j++) {
+          uint32_t coef = bs->cf2_ext[bs->hm[bi][COEFFS]][j];
+          lineqs[cnt * (bht->nv + 1) + j] = coef;
+        }
+      } else {
+        hm_t *dt = bs->hm[bi] + OFFSET;
+        for (long j = 0; j < len; j++) {
+          uint32_t coef = bs->cf2_ext[bs->hm[bi][COEFFS]][j];
+          exp_t *exp = bht->ev[dt[j]];
+          int isvar = 0;
+          for (int k = 0; k < bht->nv; k++) {
+            /* exponent vectors in hash table store the degree
+             * at the first position, thus "+1" */
+            if (exp[k + 1] == 1) {
+              lineqs[cnt * (bht->nv + 1) + k] = coef;
+              isvar = 1;
+            }
+          }
+          if (isvar == 0) {
+            lineqs[cnt * (bht->nv + 1) + bht->nv] = coef;
+          }
+        }
+        cnt++;
+      }
+    }
+  }
+  lineqs_ptr[0] = lineqs;
+}
+
 static inline void set_linear_poly(nvars_t nlins, uint32_t *lineqs, nvars_t *linvars,
                                    ht_t *bht, int32_t *bexp_lm, bs_t *bs) {
   if (bs->cf_8 != NULL) {
@@ -445,8 +519,11 @@ static inline void check_and_set_linear_poly(nvars_t *nlins_ptr, nvars_t *linvar
   } else if (bs->cf_16 != NULL) {
     check_and_set_linear_poly_16(nlins_ptr, linvars, lineqs_ptr, bht, bexp_lm,
                                  bs);
-  } else {
+  } else if (bs->cf_32 != NULL) {
     check_and_set_linear_poly_32(nlins_ptr, linvars, lineqs_ptr, bht, bexp_lm,
+                                 bs);
+  } else if (bs->cf2_ext != NULL) {
+    check_and_set_linear_poly_gf_16_256(nlins_ptr, linvars, lineqs_ptr, bht, bexp_lm,
                                  bs);
   }
 }
